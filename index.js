9158732074a36f0e1a64a1a7c3bf0fefe0e136e5
Sahil-khnn <sahil.khn338@gmail.com>
2023-07-05 16:06:02 +0500

    .

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import Connection from "./database/db.js";
+import Routes from "./routes/route.js";
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", Routes);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+const PORT = 8000;
+
+Connection();
+
+app.listen(PORT, () =>
+  console.log(`Your server is running successfully on PORT ${PORT}`)
+);
